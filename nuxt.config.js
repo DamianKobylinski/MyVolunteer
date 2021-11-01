81134ed92f19152c43fc18b6953d89e931d12b64
Jakub <jakub@Jakub-popOS.home>
2021-11-01 12:16:22 +0100

    Initial scss setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-volunteer',
+    title: 'My Volunteer',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pl'
     },
     meta: [
       { charset: 'utf-8' },
@@ -19,6 +19,12 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  
+  styleResources: {
+    scss: [
+      '@/assets/scss/main.scss'
+    ],
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -36,7 +42,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>My Volunteer</h1>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'LandingPage',
+  head () {
+    return {
+      title: 'My Volunteer'
+    }
+  }
+}
 </script>
+
+<style lang='scss' scoped>
+
+h1 {
+  font-family: 'Roboto';
+  font-weight: 500;
+}
+
+</style>
